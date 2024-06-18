6d9eb4b2ab6d267d686ac2e0982bf1be76ed016d
Cristobal Henriquez <brohenriquez@gmail.com>
2024-06-18 11:04:47 -0400

    edit CSS/js and HTML for minor changes

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,6 +5,6 @@ const lastModifiedElement = document.getElementById('lastModifiedDate');
 const lastModifiedDate = new Date(document.lastModified);
 const formattedDate = lastModifiedDate.toLocaleString();
 
-year.innerHTML = `&copy <span>${today.getFullYear()}</span> Cristobal Henriquez. Canada`;
+year.innerHTML = `&copy <span>${today.getFullYear()}</span> &#128051 Cristobal Henriquez &#128051 Canada`;
 
 lastModifiedElement.textContent = 'Last Modified: ' + formattedDate;
