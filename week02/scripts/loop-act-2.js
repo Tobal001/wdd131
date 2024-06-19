978eda0e24e37e968c73419da11cf1d3bb47ab9d
Cristobal Henriquez <brohenriquez@gmail.com>
2024-06-19 15:20:08 -0400

    created a html to practice DOM/ and loop js practice

--- week02/scripts/loop-act-2.js
+++ week02/scripts/loop-act-2.js
@@ -3,8 +3,61 @@ const LIMIT = 30;
 let studentReport = [11, 42, 33, 64, 29, 37, 44];
 
 // for loop
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+for (let i = 0; i < studentReport.length; i++) {
+    if (studentReport[i] < LIMIT) {
+        console.log(studentReport[i]);
+    }
 }
+
+//while loop
+let i = 0;
+while (i < studentReport.length) {
+    if (studentReport[i] < LIMIT) {
+        console.log(studentReport[i])    
+    }
+    i++;
+}
+
+//forEach loop
+studentReport.forEach(function (item) {
+    if (item < LIMIT) {
+        console.log(item);    
+}
+});
+
+// for...in loop
+for (let i in studentReport) {
+    if (studentReport[i] < LIMIT) {
+        console.log(studentReport[i]);
+    }
+}
+
+//LOOPING the days of the week starting from Todays date
+
+//get output location on document to append within list
+const output =
+document.getElementsByTagName("ul")
+;
+
+//Int1.DateTimeFormat Options
+const options = {weekday: 'long'};
+//vs. sort, etc.
+
+//BEGIN
+const today = new Date();
+
+//TODAY test output
+let todaystring = new Int1.DateTimeFormat('en-US', options).format(today);
+document.getElementById('today').innerHTML='Today is ${todaystring}.';
+
+//next in days (CodePen example)
+for (let i = 1; i <= n; i++ ) {
+	let nextday = new Date();
+	nextday.setDate(today.getDate() + i);
+	let nextdaystring = new Intl.DateTimeFormat('en-US', options).format(nextday);
+	item = document.createElement("li"); // list item
+	item.textContent = nextdaystring;
+	output[0].appendChild(item);
+}
+
 
